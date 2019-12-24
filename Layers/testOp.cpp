3a7b0a2f9b34d4b9b125a2eb97ed039aaadd731d
Robin Lew <yuejiancao2015@outlook.com>
2019-12-24 09:04:19 +0000

    RelU forward finished

--- Layers/testOp.cpp
+++ Layers/testOp.cpp
@@ -1,17 +1,13 @@
-#include "BasicOperations.h"
-
+//#include "BasicOperations.h"
+#include "ActivationLayers.h"
 #include <iostream>
-int main() {
+int main()
+{
   using namespace RLDNN;
-  MultiplyLayer<float, 4> mt1{};
+  RelULayer<float, 4> mt1{};
   Tensor<float, 4> a(2, 2, 2, 2);
-  Tensor<float, 4> b(2, 2, 2, 2);
   a.setRandom();
-  b.setRandom();
-  auto result = mt1.forward(OpInOutType<float,4>{{"x", a}, {"y", b}});
-  auto derivedResult = mt1.backward(result);
-  std::cout << result << std::endl;
-  std::cout << derivedResult.at("dx") << std::endl;
-  std::cout << derivedResult.at("dy") << std::endl;
+  Tensor<float, 4> out = mt1.forward(OpInOutType<float,4>{{"x",a}});
+  std::cout << out << std::endl;
   return 0;
 }
