82835dd9c559d85b80054393379122f25514d470
Robin Lew <yuejiancao2015@outlook.com>
2020-01-10 08:50:54 +0000

    change file name

---
+++ Layers/BasicOperations.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "OpInterfaces.hpp"
+namespace RLDNN {
+
+template <typename Precision, size_t Rank, Device dev=Device::CPU>
+class MultiplyLayer {
+ public:
+  MultiplyLayer() = default;
+  ~MultiplyLayer() = default;
+  Tensor<Precision, Rank> forward(const OpInOutType<Precision, Rank>& args) {
+    this->x = args.at("x");
+    this->y = args.at("y");
+    return x * y;
+  }
+  OpInOutType<Precision, Rank> backward(const Tensor<Precision, Rank>& dout) {
+    std::map<std::string_view, Tensor<Precision, Rank>> gradient;
+    gradient["dx"] = dout * this->y;
+    gradient["dy"] = dout * this->x;
+    return gradient;
+  }
+
+ private:
+  Tensor<Precision, Rank> x;
+  Tensor<Precision, Rank> y;
+};
+
+static_assert(OpValidation<MultiplyLayer<float, 4>>::valid, OP_CONCEPT_ERR);
+
+template <typename Precision, size_t Rank, Device dev=Device::CPU>
+class AddLayer {
+ public:
+  AddLayer() = default;
+  ~AddLayer() = default;
+  Tensor<Precision, Rank> forward(const OpInOutType<Precision, Rank>& args) {
+    return args["x"] + args["y"];
+  }
+  OpInOutType<Precision, Rank> backward(const Tensor<Precision, Rank>& dout) {
+    return std::map{{"dx", dout}, {"dy", dout}};
+  }
+};
+static_assert(OpValidation<AddLayer<float, 4>>::valid, OP_CONCEPT_ERR);
+
+
+}  // namespace RLDNN
---
+++ Layers/OpInterfaces.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <Eigen/Core>
+#include <boost/hana.hpp>
+#include <map>
+#include <string_view>
+#include <type_traits>
+#include <unsupported/Eigen/CXX11/Tensor>
+namespace RLDNN {
+using namespace Eigen;
+namespace hana = boost::hana;
+using namespace std::literals;
+#define OP_CONCEPT_ERR "This class does not has forward or backward function."
+
+enum Device { CPU, CUDA, NON_OPTIMIZE};
+
+template <typename Precision, size_t Rank>
+using OpInOutType = std::map<std::string_view, Tensor<Precision, Rank>>;
+
+auto hasForward = hana::is_valid(
+    [](auto&& x) -> decltype(x.forward(OpInOutType<float, 4>())) {});
+auto hasBackward =
+    hana::is_valid([](auto&& x) -> decltype(x.backward(Tensor<float, 4>())) {});
+template <typename Op>
+struct OpValidation {
+  constexpr static auto valid =
+      decltype(hasForward(Op{}))::value && decltype(hasBackward(Op{}))::value;
+};
+
+}  // namespace RLDNN
