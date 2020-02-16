07c0304a838277b7413250bedab60cfdc471f514
Robin Lew <yuejiancao2015@outlook.com>
2020-02-16 16:37:28 +0800

    fix the pointer int32_t cast bug

--- Layers/Non_Opt_Activations_C_impl.c
+++ Layers/Non_Opt_Activations_C_impl.c
@@ -1,13 +1,13 @@
 #include "Non_Opt_Activations_C_impl.h"
 
-void batchReluForward(const float* input,float* output,const int32_t* size){
-    for (int32_t i = 0; i < *size; i++)
+void batchReluForward(const float* input,float* output,const int32_t size){
+    for (int32_t i = 0; i < size; i++)
     {
         output[i]=relu_activate(*input);
     }
 }
-void batchReluBackward(const float* input,float* output,const int32_t* size){
-    for (int32_t i = 0; i < *size; i++)
+void batchReluBackward(const float* input,float* output,const int32_t size){
+    for (int32_t i = 0; i < size; i++)
     {
         output[i]=relu_gradient(*input);
     }
--- Layers/Non_Opt_Activations_C_impl.h
+++ Layers/Non_Opt_Activations_C_impl.h
@@ -4,5 +4,5 @@ static inline float relu_gradient(float x){return (x>0);}
 static inline float leaky_activate(float x,float alpha){return (x>0) ? x : alpha *x;}
 static inline float leaky_gradient(float x,float alpha){return (x>0) ? 1 : alpha;}
 
-void batchReluForward(const float* input,float* output,const int32_t* size);
-void batchReluBackward(const float* input,float* output,const int32_t* size);
+void batchReluForward(const float* input,float* output,const int32_t size);
+void batchReluBackward(const float* input,float* output,const int32_t size);
