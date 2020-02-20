b305950ec945cce3dcef0264ca98b594981a9ece
AmazingWood <amazing-wood@outlook.com>
2020-02-20 13:00:57 +0000

    externalLibs

---
+++ externalLibs.py
@@ -0,0 +1,7 @@
+import os
+class Libs:
+    def __init__(self):
+        self.boost="/home/otaku/libs/boost_1_72_0"
+        #self.boostLink="/home/otaku/libs/boost_1_72_0/stage/lib"
+        self.eigen="/home/otaku/libs/eigen-git-mirror"
+        self.mkl="/home/otaku/intel"
