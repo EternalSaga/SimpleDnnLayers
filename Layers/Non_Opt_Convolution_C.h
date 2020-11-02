77adc719699b4429076a48d8e083f390d1969820
DoctorAnn-s <52156175+DoctorAnn-s@users.noreply.github.com>
2020-11-03 05:11:45 +0800

    Update Non_Opt_Convolution_C.h

--- Layers/Non_Opt_Convolution_C.h
+++ Layers/Non_Opt_Convolution_C.h
@@ -1,9 +1,3 @@
 
 #pragma once
 #include "OpInterfaces.hpp"
-
-using Tensor1xf = Eigen::Tensor<float, 1,Eigen::RowMajor>;
-using Tensor2xf = Eigen::Tensor<float, 2,Eigen::RowMajor>;
-using Tensor3xf = Eigen::Tensor<float, 3,Eigen::RowMajor>;
-using Tensor4xf = Eigen::Tensor<float, 4,Eigen::RowMajor>;
-using Tensor5xf = Eigen::Tensor<float, 5,Eigen::RowMajor>;
